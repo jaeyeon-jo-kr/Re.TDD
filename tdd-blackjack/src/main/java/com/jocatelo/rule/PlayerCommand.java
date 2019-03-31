eaa0f4f8d83668614e1ff965026908716e74c63c
jocatelo <jojaeyeon.kr@gmail.com>
2019-03-31 20:10:50 +0900

    PlayerCommand 수정

--- tdd-blackjack/src/main/java/com/jocatelo/rule/PlayerCommand.java
+++ tdd-blackjack/src/main/java/com/jocatelo/rule/PlayerCommand.java
@@ -1,22 +1,37 @@
 package com.jocatelo.rule;
 
 import java.util.ArrayList;
-import java.util.List;
-
 import com.jocatelo.Round;
-import com.jocatelo.character.Dealer;
-import com.jocatelo.character.Playable;
 import com.jocatelo.character.Player;
 
 
 public enum PlayerCommand {
-    NONE("NONE"){public void execute(Round round, Player player){}}, 
-    //DRAW("DRAW"){public void execute(Round round, Player player){round.draw(player);}},    
-    SURRENDER("SURRENDER"){public void execute(Round round, Player player){player.setStatus(Status.SURRENDER);}},    
-    HIT("HIT"){public void execute(Round round, Player player){round.drawCard(player);}},
-    STAND("STAND"){public void execute(Round round, Player player){}},
-    DOUBLEDOWN("DOUBLEDOWN"){public void execute(Round round, Player player){}}, 
-    SPLIT("SPLIT"){public void execute(Round round, Player player){}};
+    NONE("NONE"){
+        public void execute(Round round, Player player){
+
+        }
+    },     
+    SURRENDER("SURRENDER"){
+        public void execute(Round round, Player player){
+            player.setStatus(Status.SURRENDER);
+        }
+    },    
+    HIT("HIT"){
+        public void execute(Round round, Player player){
+            round.drawCard(player);
+        }
+    },
+    STAND("STAND"){
+        public void execute(Round round, Player player){
+        }
+    },
+    DOUBLEDOWN("DOUBLEDOWN"){
+        public void execute(Round round, Player player){
+        }
+    }, 
+    SPLIT("SPLIT"){
+        public void execute(Round round, Player player){
+    }};
     private String symbol;
     PlayerCommand(String symbol){
         this.symbol = symbol;
