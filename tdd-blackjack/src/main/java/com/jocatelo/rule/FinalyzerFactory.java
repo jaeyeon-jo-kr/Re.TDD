23f27b03d3c812e89e464caf8923e387e67a89be
jocatelo <jojaeyeon.kr@gmail.com>
2019-04-08 21:50:57 +0900

    factory 리팩토링

--- tdd-blackjack/src/main/java/com/jocatelo/rule/FinalyzerFactory.java
+++ tdd-blackjack/src/main/java/com/jocatelo/rule/FinalyzerFactory.java
@@ -6,65 +6,44 @@ import com.jocatelo.rule.dealer.DealerStatus;
 
 public abstract class FinalyzerFactory {
 
+    
     static public Finalyzable create(Player player)
     {
+        
         PlayingStatus status = player.getStatus();
         switch(status)
         {
             case STAND:
-            return (Dealer dealer)->
-            {
-                if(dealer.getStatus() == DealerStatus.BLACKJACK || 
-                player.getScore() < dealer.getScore()){
-                    
-                    return WinStatus.LOSE;
-                }
-    
-                if(dealer.getScore() < player.getScore())
-                {
-                    
-                    return WinStatus.WIN;
-                }                    
-                
-                return WinStatus.DRAW;
-            };
-            case BUST:
-            return (Dealer dealer)->
-            {
-                
-                return WinStatus.LOSE;
-            };
             case PLAYING:
             return (Dealer dealer)->
             {
                 if(dealer.getStatus() == DealerStatus.BLACKJACK || 
-                player.getScore() < dealer.getScore()){
-                    
+                player.getScore() < dealer.getScore())
                     return WinStatus.LOSE;
-                }
     
                 if(dealer.getScore() < player.getScore())
-                {
-                    
                     return WinStatus.WIN;
-                }                    
-
                 return WinStatus.DRAW;
             };
+
+            case BUST:
+            return (Dealer dealer)->
+            {
+                return WinStatus.LOSE;
+            };
+
             case BLACKJACK:
             return (Dealer dealer)->
             {
                 if(dealer.getStatus() == DealerStatus.BLACKJACK)
-                {
                     return WinStatus.PUSH;
-                }
 
                 return WinStatus.BLACKJACK_WIN;
             };
+            
             case SURRENDER:
             return (Dealer dealer)->
             {
-
                 return WinStatus.LOSE;
             };
             default:
@@ -72,7 +51,5 @@ public abstract class FinalyzerFactory {
         }
         return null;
     }
-
-    
 
 }
