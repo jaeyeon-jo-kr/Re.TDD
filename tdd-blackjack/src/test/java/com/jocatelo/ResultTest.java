c4c27b74a7f07684b322d6d6cd9c567683e1c307
jocatelo <jojaeyeon.kr@gmail.com>
2019-04-10 22:46:07 +0900

    Winning Status 관련  Test 추가

--- tdd-blackjack/src/test/java/com/jocatelo/ResultTest.java
+++ tdd-blackjack/src/test/java/com/jocatelo/ResultTest.java
@@ -34,8 +34,81 @@ public class ResultTest {
         round.endGame();
 
         assertThat(WinStatus.BLACKJACK_WIN).isEqualTo(round.getWinStatus(player));
+    }
 
+    @Test
+    public void losingTest() throws Exception {
+        Round round = Round.of().setPlayerNumber(1).setAutomaticDistribute(false);
+        round.initialize();
+        round.startTurn();
+
+        Player player = round.getPlayer(0);
+        Dealer dealer = round.dealer();
+
+        dealer.addCard(Card.clover(10));
+        dealer.addCard(Card.clover(1));
+
+        player.addCard(Card.clover(10));
+        player.addCard(Card.clover(10));
         
+        player.setCommand(PlayerCommand.STAND);
+        player.execute();
+
+        round.updateAllStatus();        
+        round.endTurn();
+        round.endGame();
+
+        assertThat(WinStatus.LOSE).isEqualTo(round.getWinStatus(player));
+
+    }
+
+    @Test
+    public void pushTest() throws Exception {
+        Round round = Round.of().setPlayerNumber(1).setAutomaticDistribute(false);
+        round.initialize();
+        round.startTurn();
+
+        Player player = round.getPlayer(0);
+        Dealer dealer = round.dealer();
+
+        dealer.addCard(Card.clover(10));
+        dealer.addCard(Card.clover(1));
+
+        player.addCard(Card.clover(10));
+        player.addCard(Card.clover(1));
         
+        player.setCommand(PlayerCommand.STAND);
+        player.execute();
+
+        round.updateAllStatus();        
+        round.endTurn();
+        round.endGame();
+
+        assertThat(WinStatus.PUSH).isEqualTo(round.getWinStatus(player));
+    }
+
+    @Test
+    public void drawTest() throws Exception {
+        Round round = Round.of().setPlayerNumber(1).setAutomaticDistribute(false);
+        round.initialize();
+        round.startTurn();
+
+        Player player = round.getPlayer(0);
+        Dealer dealer = round.dealer();
+
+        dealer.addCard(Card.clover(10));
+        dealer.addCard(Card.clover(9));
+
+        player.addCard(Card.clover(10));
+        player.addCard(Card.clover(9));
+        
+        player.setCommand(PlayerCommand.STAND);
+        player.execute();
+
+        round.updateAllStatus();        
+        round.endTurn();
+        round.endGame();
+
+        assertThat(WinStatus.DRAW).isEqualTo(round.getWinStatus(player));
     }
 }
